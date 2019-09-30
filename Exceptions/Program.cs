e19e9ce4a5f11238c38e060580600fed03dcc686
liga.gzibovska@inbox.lv <liga.gzibovska@inbox.lv>
2019-09-30 20:55:21 +0300

    Added exceptions

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -10,7 +10,50 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            try
+            {
 
+                int num1 = GetNumber("exter number a");
+                int num2 = GetNumber("exter number b");
+
+                Console.WriteLine("division= {0}",
+                    Calculator.Divide(num1, num2));
+                               
+            }
+
+            catch(CalculatorException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            catch(Exception)
+            {
+                Console.WriteLine("neparedzeta kluda");
+            }
+            
+            Console.Read();
+        }
+
+        static int GetNumber(string text)
+        {
+            Console.Write(text);
+            string input = Console.ReadLine();
+
+         //   if(int.TryParse(input, out int result))
+          //  {
+         //       return result;
+        //    }
+
+            try
+            {
+                int result = int.Parse(input);
+                return result;
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("invalid number" );
+                return GetNumber(text);
+            }
         }
     }
 }
