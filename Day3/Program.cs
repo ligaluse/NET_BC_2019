eb53e33d7bfa37a2db5654ab0394d2839c92e98c
ligaluse <55846732+ligaluse@users.noreply.github.com>
2019-10-02 22:08:54 +0300

    Add files via upload

---
+++ Day3/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.WriteLine("enter your full name: ");
+            string FullName = Console.ReadLine();
+
+            var userList = new UserList(string FullName, );
+
+            if (FullName == null
+            {
+                Console.WriteLine("user not found");
+               List<UserList> userList = Add.ToList(FullName);
+            }
+            )
+
+            
+
+            Console.WriteLine("enter your gender(m/f): ");
+            enum Gender = enum.TryParse(Console.ReadLine());
+
+            Console.WriteLine("enter your date of birth: ");
+            Datetime dateOfBirth = Console.ReadLine();
+
+        }
+    }
+}
---
+++ Day3/UserProfile.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day3
+{
+    class UserProfile
+    {
+        string FullName;
+        enum Gender
+        {
+            m,
+            f
+        }
+        DateTime dateOfBirth;
+    }
+
+    public static int CalculateAge(DateTime dateOfBirth)
+    {
+        int age = 0;
+        age = DateTime.Now.Year - dateOfBirth.Year;
+        return age;
+    }
+}
