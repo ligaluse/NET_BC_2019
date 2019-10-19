87143134abe1099e2f7b0f793563e9b9e3107437
ligaluse <55846732+ligaluse@users.noreply.github.com>
2019-10-19 10:52:05 +0300

    Add files via upload

---
+++ LogicSample.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("LogicSample.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("LogicSample.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f6df0f3e-e96f-444d-8422-9fb55bf6a9fe")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ LogicSample.Tests/UnitTestUser.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LogicSample.Tests
+{
+    [TestClass]
+    public class UnitTestUser
+    {
+        [TestMethod]
+        public void Test_GetAllUsers()
+        {
+            UserManager manager = new UserManager();
+            manager.Seed();
+            var result = manager.GetAll();
+
+            //sagaidītā vērtība un reālā vērtība, lai tests būtu veiksmīgs
+            Assert.AreEqual("Name 1", result[0].Name);
+            Assert.AreEqual(2, result.Count);
+        }
+
+        [TestMethod]
+        public void TestCreateUser()
+        {
+            UserManager manager = new UserManager();
+            User user = manager.CreateNew(new User()
+            {
+                Name = "new name"
+            });
+            Assert.AreEqual("new name", user.Name);
+            Assert.IsTrue(user.Id > 0);
+        }
+        [TestMethod]
+        public void TestGetUser()
+        {
+            UserManager manager = new UserManager();
+            manager.Seed();
+            User user1 = manager.Get(1);
+            User user2 = manager.Get(2);
+            User user3 = manager.Get(3);
+
+            Assert.AreEqual("Name 1", user1.Name);
+            Assert.AreEqual("Name 2", user2.Name);
+            Assert.IsNull(user3);
+        }
+        [TestMethod]
+        public void TestUpdateUser()
+        {
+            UserManager manager = new UserManager();
+            manager.Seed();
+            manager.Update(new User()
+            {
+                Id =2,
+                Name = "new name"
+            });
+            var user = manager.Get(2);
+            Assert.AreEqual(2, user.Id);
+            Assert.AreEqual("new name", user.Name);
+        }
+        [TestMethod]
+        public void TestDeleteUser()
+        {
+            UserManager manager = new UserManager();
+            manager.Seed();
+            manager.Delete(1);
+
+            var allUsers = manager.GetAll();
+            var deletedUser = manager.Get(1);
+
+            Assert.AreEqual(1, allUsers.Count);
+            Assert.IsNull(deletedUser);
+        }
+    }
+}
